6880aae3aac8457c5ef74500c7bdf3e35b7aa619
hyunsooryu <41038031+hyunsooryu@users.noreply.github.com>
2021-12-05 00:19:37 +0900

    2021-12-05 Instagram Clone With Flutter LayOut

---
+++ android/app/src/main/kotlin/me/hyunsoo/instagramclone/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.hyunsoo.instagramclone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  HomePage({
+    Key? key,
+  }) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<BottomNavigationBarItem> btmNavItems = [
+     BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+     BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+     BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
+     BottomNavigationBarItem(icon: Icon(Icons.healing), label: ''),
+     BottomNavigationBarItem(icon: Icon(Icons.account_circle), label:''),
+  ];
+
+  int _selectedIndex = 0;
+
+  static List<Widget> _screens = <Widget>[
+    Container(color : Colors.amberAccent),
+    Container(color : Colors.blueAccent),
+    Container(color : Colors.greenAccent),
+    Container(color : Colors.deepPurpleAccent),
+    Container(color : Colors.cyanAccent)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        appBar: AppBar(
+          title: const Text("cool App"),
+        ),
+
+        body: IndexedStack(
+          index : _selectedIndex,
+          children: _screens
+        ),
+
+        bottomNavigationBar: BottomNavigationBar(
+          items: btmNavItems,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor : Colors.black87,
+          currentIndex: _selectedIndex,
+          onTap: _onBtmItemClick,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+        ),
+    );
+  }
+
+  void _onBtmItemClick(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
